39f6dbafb4a2cef64d82922e9c36bf739f00b8b4
Alexis Métaireau <alexis@notmyidea.org>
2016-06-23 11:31:42 +0200

    Have a first working version

--- catalog.py
+++ catalog.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import os
 from uuid import uuid4
 
@@ -10,7 +10,7 @@ HERE = os.path.dirname(__file__)
 
 class Catalog(object):
 
-    def __init__(self, title, packager_name, author_name=None,
+    def __init__(self, title, packager_name=None, author_name=None,
                  author_uri=None):
         """Generates a catalog for the given passed data.
 
@@ -18,9 +18,6 @@ class Catalog(object):
 
         :param title:
             The title of the catalog in human-readable form.
-        :param packager_name:
-            The name of the packager (most probably the entity running this
-            script).
 
         Optional parameters:
 
@@ -31,26 +28,29 @@ class Catalog(object):
             The name of the author.
         :param author_uri:
             An URI to retrieve more information about the author.
+        :param packager_name:
+            The name of the packager (most probably the entity running this
+            script).
         """
         self.title = title
         self.packager_name = packager_name
         self.author_name = author_name
         self.author_uri = author_uri
+        self.entries = []
 
-    def add_entry(self, title, issued, urls, id=None, summary=None,
+    def add_entry(self, title, issued, urls, uuid=None, summary=None,
                   author_name=None, author_uri=None, checksum=None,
-                  file_size=None, tags=None, requires_mimetypes=None,
-                  image=None, thumbnail=None, languages=("eng", ),
-                  checksum_algorithm="sha256"):
+                  file_size=None, tags=(), requires_mimetypes=(),
+                  image=None, thumbnail=None, updated=None,
+                  rights=(), languages=('eng', ), checksum_algorithm='sha256'):
         """Adds an entry to the catalog.
 
         Required parameters:
 
-        XXX: Should add this.
-        A human readable identifier for the resource. It's the same
-        across versions (should be stable across time).
-        MUST be prefixed by the packager name.
-
+        :param id:
+            A human readable identifier for the resource. It's the same
+            across versions (should be stable across time).
+            MUST be prefixed by the packager name.
         :param title:
             The title of the resource.
         :param issued:
@@ -61,7 +61,7 @@ class Catalog(object):
 
         Optional keys:
 
-        :param id:
+        :param uuid:
             A unique identifier for the resource. (defaults to a generated UUID
             if not specified).
         :param summary:
@@ -88,12 +88,20 @@ class Catalog(object):
             An URI to an image (large) representation of the content.
         :param thumbnail:
             An URI to a thumbnail (small) representation of the content.
+        :param updated:
+            A python datetime object describing when the resource was last
+            updated by its producer (defaults to the current date)
+        :param rights:
+            A list of rights associated with the content.
         """
         self.entries.append(dict(
+            id=id,
+            uuid=uuid,
             title=title,
             issued=issued,
+            updated=updated or issued,
+            rights=rights,
             urls=urls,
-            id=id,
             summary=summary,
             author_name=author_name,
             author_uri=author_uri,
@@ -107,18 +115,27 @@ class Catalog(object):
             checksum_algorithm=checksum_algorithm))
 
 
-def export_as_opds(catalog, url=None, root_url=None, updated=None, uuid=None):
+def to_opds(catalog, url, root_url=None, updated=None, uuid=None):
     """Exports the given catalog in the OPDS format.
+
+    XXX Find how to handle the "id" in an OPML compatible way.
+    XXX Check that entry.issued actually means the last version (it might be
+        xzthe first date of issuance)
+
+    Required params:
 
     :param catalog:
         The catalog object.
+    :param url:
+        Absolute URL of the current catalog.
+
+    Optional params:
+
     :param uuid:
         The unique id of the feed.
     :param updated:
         A python date object representing the feed generation time
         (defaults to now).
-    :param url:
-        Absolute URL of the current catalog.
     :param root_url:
         Absolute URL to the root catalog (defaults to the same value
         url is providing).
@@ -128,10 +145,24 @@ def export_as_opds(catalog, url=None, root_url=None, updated=None, uuid=None):
     root_url = root_url or url
 
     env = Environment(loader=FileSystemLoader(HERE))
-    template = env.get_template("catalog.opds.jinja2")
+    env.filters['mimetype'] = guess_image_mimetype
+    template = env.get_template('catalog.opds.jinja2')
     return template.render(
         catalog=catalog,
         uuid=uuid,
         updated=updated,
         url=url,
         root_url=root_url)
+
+
+def guess_image_mimetype(filename):
+    """Detects the mimetype of a URI from the filename."""
+    ext = filename.split('.')[-1].lower()
+    mt = 'img/jpg'  # jpg is returned by default.
+    if ext in ('jpg', 'jpeg'):
+        mt = 'image/jpeg'
+    elif ext in ('gif', ):
+        mt = 'image/gif'
+    elif ext in ('png'):
+        mt = 'image/png'
+    return mt
